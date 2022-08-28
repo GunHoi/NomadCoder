73a9ad1897704ec62bff3344532fdc1ce9e139b0
GunHoi <lgh509@kyonggi.ac.kr>
2022-08-28 21:49:21 +0900

    바닐라 JS로 크롬 앱 만들기 Lecture 08 API를 이용한 위치,날씨 정보 받아오기

---
+++ Vanilla_JS/momentum/js/weather.js
@@ -0,0 +1,25 @@
+const API_KEY = " ";    //본인 API key값 입력
+
+function onGeoSuccess(position){
+    const lat = position.coords.latitude;  //latitude
+    const lon = position.coords.longitude;;  //longitude
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url).then(response => response.json()).then(date => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:first-child");
+        weather.innerText = data.weather[0].main;
+        city.innetText =  data.name;
+    }); 
+}
+/*fetch를 통해 url을 불러올 수 있다. 
+fetch는 promise로, 당장 무언가가 일어나는 것이 아니라, 시간이 좀 걸린 후 에 일어난다.
+서버의 응답을 기다리고 난 후 ~를 해라 -> .then()을 사용한다.
+response.json -> url을 입력했을 때 나오는 정보들 전부를 의미한다.
+*/
+function onGeoError(){
+    alert("Cant find you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
+//user의 위치를 받아올 수 있다. (받아오는데 성공하면 onGeoSuccess함수를,
+// 실패하면 onGeoError을 받아온다.)
